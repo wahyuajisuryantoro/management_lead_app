b130e4833ea893d2fca0d079c7dc436cef847f3b
wahyuajisuryantoro <ajiwahyu334@gmail.com>
2026-02-18 14:36:57 +0700

    fixing assets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -59,19 +59,14 @@
         </div>
     </div>
     <div class="dark-transparent sidebartoggler"></div>
-    <!-- Import Js Files -->
     <script src="{{ asset('assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
     <script src="{{ asset('assets/libs/simplebar/dist/simplebar.min.js') }}"></script>
+
     <script src="{{ asset('assets/js/theme/app.init.js') }}"></script>
     <script src="{{ asset('assets/js/theme/theme.js') }}"></script>
     <script src="{{ asset('assets/js/theme/app.min.js') }}"></script>
     <script src="{{ asset('assets/js/theme/sidebarmenu-default.js') }}"></script>
 
-    <!-- solar icons -->
-    <script src="https://cdn.jsdelivr.net/npm/iconify-icon@1.0.8/dist/iconify-icon.min.js"></script>
-    <script src="{{ asset('assets/libs/apexcharts/dist/apexcharts.min.js') }}"></script>
-    <script src="{{ asset('assets/js/dashboards/dashboard1.js') }}"></script>
-    <script src="{{ asset('assets/libs/fullcalendar/index.global.min.js') }}"></script>
 </body>
 
 </html>
